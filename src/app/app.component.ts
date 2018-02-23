eed08ba996fc6c8ed357fba1bec48ed99fc9ae07
MMA <mma@mma.pl>
2018-02-23 19:02:00 +0100

    Adding temp menu navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  menuItem: string = 'recipes';
+
+  onMenuChoose(menuItem: string) {
+    this.menuItem = menuItem;
+  }
 }
